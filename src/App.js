40379b9e9e82478f3b1360e85eb0408f3da80c78
AbduEndrisM <beazsasm@gmail.com>
2019-03-04 00:00:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {Layout, Header, Navigation,Drawer, Content} from 'react-mdl';
 import Main from './components/main';
 class App extends Component {
   render() {
-    return (
+    return ( 
       /* Uses a header that scrolls with the text, rather than staying locked at the top */
 <div className="demo-big-content">
     <Layout>
