2b888128c0f2ae88a397fe3d998a88629442cfc9
AbduEndrisM <beazsasm@gmail.com>
2019-03-04 23:53:17 -0600

    Login page

---
+++ src/components/UploadScreen.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+// import axios from 'axios;'
+class UploadScreen  extends Component{
+    render(){
+        return(
+            <div> <h1> Upload Screen   </h1> </div>
+        )
+    }
+}
+
+export default UploadScreen ;
