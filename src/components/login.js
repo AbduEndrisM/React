ca01f3464ffcf855c8c82dc41eb3bb04a5594a12
AbduEndrisM <beazsasm@gmail.com>
2019-03-04 22:43:49 -0600

    Login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,42 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Login extends Component{
-    render(){
-        return(
-            <div> <h1> Login </h1> </div>
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value.toUpperCase());
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div> <h1> Login </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+
+                    <div>
+                        <textarea>
+                            Hello there, this is some text in a text area
+                        </textarea>
+                    </div>
+                </form>
+            </div>
+
         )
     }
 }
